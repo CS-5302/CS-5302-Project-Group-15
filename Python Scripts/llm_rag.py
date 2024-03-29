7adc7a4e2e6b21f537cf59a4a95643a55640a71a
Talha Ahmed <24100033@lums.edu.pk>
2024-03-29 23:39:56 +0500

    update

---
+++ Python Scripts/llm_rag.py
@@ -0,0 +1,27 @@
+# State neccessary import + installation
+
+
+# Step 1:
+"""
+Reading the document and get service context of llm
+"""
+
+# Step 2:
+"""
+Make index in vector database and storage context
+"""
+
+# Step 3:
+"""
+Get retrieval or make it if non-existent (most time consuming imo)
+"""
+
+# Step 4:
+"""
+Bot Engine Creation (involves creation of template)
+"""
+
+# Step 5:
+"""
+Give Query --> Get Response
+"""
