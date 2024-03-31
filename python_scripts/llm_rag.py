85d749d8967f91b87d7e8300d1c5829a26404135
Talha Ahmed <24100033@lums.edu.pk>
2024-03-31 20:05:59 +0500

    update

--- python_scripts/llm_rag.py
+++ python_scripts/llm_rag.py
@@ -16,6 +16,121 @@ from llama_index.core import PromptTemplate
 from IPython.display import Markdown, display
 import chromadb
 
+
+
+
+def setup_environment(api_key, model_version = "gpt-3.5-turbo"):
+    """
+    Setup the environment by initializing OpenAI and service context.
+    
+    :param api_key: OpenAI API Key for authentication
+    :param model_version: Version of the OpenAI model to use
+    :return: Configured service context
+    """
+    os.environ["OPENAI_API_KEY"] = api_key
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    # Initialize service context with OpenAI settings
+    service_context = Settings
+    service_context.llm = OpenAI(model=model_version)
+    service_context.embed_model = OpenAIEmbedding(model = "text-embedding-3-small")
+    service_context.node_parser = SentenceSplitter(chunk_size = 512, chunk_overlap = 20)
+    service_context.num_output = 512
+    service_context.context_window = 3900
+
+    return service_context
+
+def prepare_documents(path, collection_name, persistent = False, PATH = None):
+    """
+    Prepare the documents by loading them and initializing the ChromaDB collection.
+
+    :param path: Path to the directory containing the documents
+    :param collection_name: Name of the collection to create in ChromaDB
+    :param persistent: Boolean flag to indicate whether to use persistent or ephemeral ChromaDB
+    :param PATH: Path to the directory containing the ChromaDB data. None if persistent = False
+    :return: Tuple of loaded documents and the initialized ChromaDB collection
+    """
+    
+    if persistent:
+        # Intialize chromaDB from memory
+        chroma_client = chromadb.PersistentClient(path = PATH)
+    else:
+        # Initialize ChromaDB client and create a new collection
+        chroma_client = chromadb.Client()
+        chroma_collection = chroma_client.create_collection(name = collection_name, metadata = {"hnsw:space": 'cosine'})
+    
+    # Load documents from the specified directory
+    documents = SimpleDirectoryReader(path).load_data()
+    return documents, chroma_collection
+
+def embed_and_index(documents, chroma_collection, model_name = "BAAI/bge-base-en-v1.5"):
+    """
+    Embed the documents and build an index in the vector database.
+
+    :param documents: List of documents to process
+    :param chroma_collection: ChromaDB collection object
+    :param model_name: Model name for the embedding model
+    :return: Built index object
+    """
+    embed_model = HuggingFaceEmbedding(model_name = model_name)
+    chunks = service_context.node_parser(documents)
+
+    # Process each chunk of the document
+    texts, text_embeds, metadatas = [], [], []
+    for chunk in chunks:
+        texts.append(chunk.text)
+        text_embeds.append(embed_model.get_text_embedding(chunk.text))
+        metadatas.append({'source': collection_name, 'text': chunk.text})
+
+    ids = [str(uuid4()) for _ in range(len(text_embeds))]
+
+    # Add processed data to the ChromaDB collection
+    chroma_collection.add(embeddings=text_embeds, documents=texts, metadatas=metadatas, ids=ids)
+
+    # Create a vector store and a storage context for indexing
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection, add_sparse_vector=True, include_metadata=True)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    # Build the index using the processed documents
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
+    return index
+
+def query_data(index, query):
+    """
+    Query data from the index and display the result.
+
+    :param index: The index object to query from
+    :param query: The query string
+    """
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+    display(Markdown(f"<b>{response}</b>"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Setup environment variables
 # os.environ["OPENAI_API_KEY"] = getpass.getpass("OpenAI API Key:")
 # openai.api_key = os.environ["OPENAI_API_KEY"]
---
+++ python_scripts/machine_translation.py
@@ -0,0 +1,38 @@
+# Neccessary Installations
+# !pip install sentencepiece
+
+from transformers import MarianMTModel, MarianTokenizer
+
+import warnings
+warnings.filterwarnings('ignore')
+
+def translate_text(text, src_lang, trg_lang):
+    model_name = f'Helsinki-NLP/opus-mt-{src_lang}-{trg_lang}'
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+
+    # tokenizing the text
+    tokenized_text = tokenizer.prepare_seq2seq_batch([text], return_tensors='pt')
+
+    # generating the translation
+    translated = model.generate(**tokenized_text)
+
+    # decoding the translation
+    translation = tokenizer.batch_decode(translated, skip_special_tokens = True)
+
+    return translation[0]
+
+# Example Usage
+
+sample_text = "I live in Pakistan"
+
+# English to Arabic translation
+translated_text = translate_text(sample_text, "en", "ar")
+print(translated_text)
+
+"""
+>>> أعيش في باكستان
+"""
+
+# Note will take time for a new language as it initializes the model etc. Japanese and Arabic have been done
+# but accuracy has to be checked.
